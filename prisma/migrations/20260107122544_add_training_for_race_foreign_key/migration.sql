9bcbe357f0b3daea9988902b3802549f2d523080
Adam Cole <adam@gofastcrushgoals.com>
2026-01-08 22:23:24 -0500

    Add missing migration.sql file for trainingForRace foreign key

---
+++ prisma/migrations/20260107122544_add_training_for_race_foreign_key/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "run_crews" ADD CONSTRAINT "run_crews_trainingForRace_fkey" FOREIGN KEY ("trainingForRace") REFERENCES "race_registry"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
