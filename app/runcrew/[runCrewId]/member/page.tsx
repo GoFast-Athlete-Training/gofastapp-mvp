5fa4bb5b30aeee4c48d6136b6c6d76db1b31d1cd
Adam Cole <adam@gofastcrushgoals.com>
2026-01-16 21:39:36 -0500

    Fix TypeScript error: convert null to undefined for currentUserId prop

--- app/runcrew/[runCrewId]/member/page.tsx
+++ app/runcrew/[runCrewId]/member/page.tsx
@@ -312,7 +312,7 @@ export default function RunCrewMemberPage() {
                         joinedAt: membershipItem.joinedAt,
                       }}
                       showRole={true}
-                      currentUserId={LocalStorageAPI.getAthleteId()}
+                      currentUserId={LocalStorageAPI.getAthleteId() ?? undefined}
                     />
                   ))}
                 </div>
